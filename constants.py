5e3e8c5738b3572f0f9b806302e071d029ec6693
Ruiqi Yu <37889326+ruiqiy4-1663775@users.noreply.github.com>
2023-11-27 22:51:41 -0800

    Q_learning agent. Reformat the folder

---
+++ constants.py
@@ -0,0 +1,47 @@
+INGREDIENTS = [
+    "Red Onions",
+    "Yellow Onions",
+    "Bread",
+    "Yellow Cheese",
+    "White Cheese",
+    "Red Bell Pepper",
+    "Green Bell Pepper",
+    "Red Apple",
+    "Green Apple",
+    "Yellow Egg",
+    "White Egg",
+    "Butter",
+    "Ham",
+    "Red Meat (Beef)",
+    "White Meat (Turkey)",
+    "Salt",
+    "Herbs"
+]
+
+KITCHEN_TOOLS = [
+    "Plate",
+    "Pot",
+    "Oven",
+    "Knife",
+    "Mixing Bowl"
+]
+
+ALL_DESTINATIONS = INGREDIENTS + KITCHEN_TOOLS
+
+MEDIUM_LEVEL_ACTIONS = [
+    "Go to",
+    "Use",
+    "Prepare",
+    "Take",
+    "Drop",
+    "Place"
+]
+
+LOW_LEVEL_ACTIONS = [
+    'right',
+    'left',
+    'down',
+    'up',
+    'take',
+    'prepare and place'
+]
